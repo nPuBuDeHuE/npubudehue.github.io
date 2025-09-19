01c45c051cb4e36c7b183823a11e39798c23063b
Vitaly Bykov <bykvitaly@yandex.ru>
2025-09-19 22:25:17 +0500

    добавлена русская версия на главную страницу и возможность переключать язык

---
+++ lang/translate.js
@@ -0,0 +1,30 @@
+export const LANG = {
+    es: {
+        "menu_alias":"Vitaly 'Toros' Bykov",
+        "menu_work": "Mis trabajos",
+        "menu_info": "Información",
+        "about_me":"<p>¡Hola! Soy Vitali.</p><p>Me dedico al desarrollo frontend y a la maquetación de páginas web a partir de diseños (no soy diseñador).</p><p>Mi apellido 'Býkov' significa 'Toro', por eso tengo este alias y avatar :)</p><p>Aquí hay ejemplos de mi trabajo y mis datos de contacto.</p><p>¡Escríbeme, estaré encantado de ayudar!</p>",
+        "projects_header":"trabajos",
+        "12_chairs": "Las doce sillas",
+        "12_chairs_details": `<p>Esta página está inspirada en la novela <a target='_blank' href='https://es.wikipedia.org/wiki/Las_doce_sillas_(novela)'>Las doce sillas</a>.</p>
+        <p>Aquí podemos ver la publicidad del famoso torneo de ajedrez con la participación de Ostap Bender y renombrados grandes maestros mundiales.</p>
+        <p>El libro fue escrito a principios del siglo XX y narra la búsqueda de los diamantes de una difunta señora. Es una obra satírica y de aventuras.</p>
+        <p>La novela cuenta con traducción al español, así como con adaptaciones cinematográficas en español.</p>`,
+        "proxima": "Próximamente..",
+        "footer_figma": "Maquetado a partir de Figma"
+    }, 
+    ru: {
+        "menu_alias":"Виталий 'Toros' Быков",
+        "menu_work": "Мои работы",
+        "menu_info": "Информация",
+        "about_me":"<p>Привет! Я - Виталий.</p><p>Я frontend-разработчик и html-верстальщик (не дизайнер).</p><p>Мне нравится Испания и испанский язык, поэтому я взял ник, который перекликается с моей фамилией (toro по-испански - бык :)</p><p>Здесь представлены мои работы. Если вам нужна помощь с версткой - я готов помочь.</p>",
+        "projects_header":"работы",
+        "12_chairs": "Двенадцать стульев",
+        "12_chairs_details": `<p>Эта страничка вдохновлена романом <a href='https://ru.ruwiki.ru/wiki/Двенадцать_стульев'>Двенадцать стульев</a></p>
+        <p>Она переносит нас к международному шахматному турниру в Нью-Васюках с участием знаменитых международных гроссмейстеров и Остапа Бендера.</p>
+        <p>Книга была написана в начале XX века и рассказывает о поисках богатства умершей дворянки. Представляет собой сатирическое и приключенческое произведение.</p>
+        <p>Произведение неоднократно экранизировалось. Мне больше всего нравится <a href='https://ru.ruwiki.ru/wiki/12_стульев_(фильм,_1976)'>версия 1976 года</a> с <a href='https://ru.ruwiki.ru/wiki/Миронов,_Андрей_Александрович'>Андреем Мироновым</a>, которую поставил <a href='https://ru.ruwiki.ru/wiki/Захаров,_Марк_Анатольевич'>Марк Захаров</a> </p>`,
+        "proxima": "Скоро..",
+        "footer_figma": "Свёрстано по макету Figma"
+    }
+}
--- script/script.js
+++ script/script.js
@@ -1,5 +1,8 @@
+import { LANG } from "../lang/translate.js";
+
 window.addEventListener('DOMContentLoaded', function() {
     console.log("DOM загружен");
+    console.log("LANG -- ", LANG);
     
     const whatsButton = document.querySelector(".whatsapp_link");
     if (whatsButton) {
@@ -7,10 +10,45 @@ window.addEventListener('DOMContentLoaded', function() {
     } else {
         console.error("Элемент .whatsapp_link не найден");
     }
+
+    const langSelectWatch = document.querySelector(".menu_lang_select");
+    if (langSelectWatch){
+        langSelectWatch.addEventListener("click", changeLanguage);
+    } else {
+        console.error("Элемент .menu_lang_select не найден")
+    }
 });
 
 function openWhatsapp(event) {
     event.preventDefault(); // Предотвращаем стандартное поведение ссылки
     console.log('whatsapp link clicked');
     window.open('https://wa.me/79177994693', '_blank');
+}
+
+function changeLanguage(flag_current) {
+    const flag_es = document.querySelector(".estandarte_espana");
+    const flag_ru = document.querySelector(".estandarte_rusia");
+    const elements = document.querySelectorAll('[data-translate]');
+    let selectedLanguage; 
+
+    if (flag_current.target == flag_es) {
+        flag_es.classList.add('hidden');
+        flag_ru.classList.remove('hidden');
+        selectedLanguage = flag_ru.dataset.lang;
+
+    } else if (flag_current.target == flag_ru) {
+        flag_ru.classList.add('hidden');
+        flag_es.classList.remove('hidden');
+        selectedLanguage = flag_es.dataset.lang;
+    }
+
+    console.log('selectedLanguage', selectedLanguage);
+
+    elements.forEach(element => {
+        const key = element.getAttribute('data-translate');
+        if (LANG[selectedLanguage] && LANG[selectedLanguage][key]) {
+            // Используем innerHTML вместо textContent для интерпретации HTML
+            element.innerHTML = LANG[selectedLanguage][key];
+        }
+    });
 }
