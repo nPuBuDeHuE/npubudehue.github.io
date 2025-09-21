2ff1acdb22d85a5482eed300dd8c22e71094ca21
Vitaly Bykov <bykvitaly@yandex.ru>
2025-09-22 00:20:02 +0500

    работа с меню (стили, атоматическая загрузка по данным из resources.js)

---
+++ projects/taste/js/resources.js
@@ -0,0 +1,74 @@
+export const MENU = {
+    "Starters": [
+        {
+            'name':'Raw Scallops from Erquy', 
+            'ingridients': 'Shuck the scallop to that used for oysters',
+            'price': '$40',
+            'img_name': 'starters_raw.jpg',
+        },
+        {
+            'name':'Spring Roll', 
+            'ingridients': 'Add oil to a hot pan spring onion whites',
+            'price': '$20',
+            'img_name': 'starters_roll.jpg',
+        },
+        {
+            'name':'French Onion Soup', 
+            'ingridients': 'Wheat flour, apple cider vinegar, bread',
+            'price': '$25',
+            'img_name': 'starters_soup.jpg',
+        },
+        {
+            'name':'Tomato Bruschetta', 
+            'ingridients': 'Bread, olive oil, garlic, black pepper',
+            'price': '$30',
+            'img_name': 'starters_bruschetta.jpg',
+        }
+    ], 
+    
+    "Main Dish": [
+        {
+            'name':'Apple Pie with Cream', 
+            'ingridients': 'Whipping cream, egg white, cinnamon',
+            'price': 15,
+            'img_name': 'dessert_apple.jpg',
+        },
+        {
+            'name':'Lemon Meringue Pie', 
+            'ingridients': 'Frozen pie crust, meringue, lemon',
+            'price': 30,
+            'img_name': 'dessert_lemon.jpg',
+        },
+        {
+            'name':'Apple Pie with Cream', 
+            'ingridients': 'Whipping cream, egg white, cinnamon',
+            'price': 15,
+            'img_name': 'dessert_apple.jpg',
+        },
+        {
+            'name':'Lemon Meringue Pie', 
+            'ingridients': 'Frozen pie crust, meringue, lemon',
+            'price': 30,
+            'img_name': 'dessert_lemon.jpg',
+        }
+    ], 
+    
+    "Dessert": [
+        {
+            'name':'Apple Pie with Cream', 
+            'ingridients': 'Whipping cream, egg white, cinnamon',
+            'price': 15,
+            'img_name': 'dessert_apple.jpg',
+        },
+        {
+            'name':'Lemon Meringue Pie', 
+            'ingridients': 'Frozen pie crust, meringue, lemon',
+            'price': 30,
+            'img_name': 'dessert_lemon.jpg',
+        }
+
+    ]
+}
+
+
+
---
+++ projects/taste/js/script.js
@@ -0,0 +1,38 @@
+import { MENU } from "./resources.js";
+
+window.addEventListener('DOMContentLoaded', function() {
+    console.log("DOM загружен");
+    makeMenu(MENU)
+});
+
+function makeMenu(menuList) {
+    const menuHome = document.querySelector(".foodmenu_mainmenu");
+
+    for (let key in menuList) {
+        if (menuList.hasOwnProperty(key)) {
+            const menuBlock = document.createElement("section");
+            menuBlock.classList.add("starters_section");
+            menuHome.appendChild(menuBlock);
+            const menuBlockHeader = document.createElement("p");
+            menuBlockHeader.classList.add("header36");
+            menuBlockHeader.classList.add("starters_header");
+            menuBlockHeader.textContent = key;
+            menuBlock.appendChild(menuBlockHeader);
+            for (let i = 0; i < menuList[key].length; i++) {
+                const item = menuList[key][i];
+                const elem = `
+                <div class="menu_item">
+                    <img src="img/menu/${item.img_name}" alt="" class="menu_item__img">
+                    <div class="menu_item__details">
+                            <p class="menu_item__details_header header30">${item.name}</p>
+                            <p class="menu_item__details_text">${item.ingridients}</p>
+                        </div>
+                        <div class="menu_item__line"></div>
+                        <div class="menu_item__price header30">${item.price}</div>
+                    </div>
+                `;
+                menuBlock.insertAdjacentHTML('beforeend', elem);
+            }
+        }
+    }
+}
